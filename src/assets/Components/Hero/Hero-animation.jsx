cafc1f10cd5a9070775537e1be61ad9ca419f7c7
Venkatesh124rayudu <venkateshrayudu124@gmail.com>
2025-02-22 17:19:10 +0530

    Added the Hero section

---
+++ src/assets/Components/Hero/Hero-animation.jsx
@@ -0,0 +1,16 @@
+import "./Hero-animation.css";
+const HeroAnimation = ()=>{
+    return(
+        <><div className="heroAnimation">
+        <div className="loader">
+            <div className="box"></div>
+            <div className="box"></div>
+            <div className="box"></div>
+            <div className="box"></div>
+            <div className="box"></div>
+          </div>
+        </div>
+        </>
+    )
+}
+export default HeroAnimation;
---
+++ src/assets/Components/Hero/Hero-image.jsx
@@ -0,0 +1,10 @@
+import Karthik from "../../Images/Karthik.png";
+import './Hero-image.css';
+const HeroImg=()=>{
+    return (<>
+        <div className="Hero-imgs col-5">
+              <img src={Karthik} alt="Karthik"></img>
+            </div>
+    </>)
+}
+export default HeroImg;
