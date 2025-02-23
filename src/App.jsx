fc05aa4887328a63c8926ed37647132daa8e3f1e
Karthikramesh12 <133801320+Karthikramesh12@users.noreply.github.com>
2025-02-23 13:02:06 +0530

    added services page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import './App.css'
 import LoadingScreen from './Components/Loadin Screen/LoadingScreen'
+import Services from './Components/Services/Services'
 function App() {
  
 
   return (
     <>
-    
       <LoadingScreen/>
-      
+      <Services/>
     </>
   )
 }
