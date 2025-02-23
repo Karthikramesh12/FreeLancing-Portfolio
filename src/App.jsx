bc7df56e26b35bb68eb094c34a6bad6c5759f4e6
Karthikramesh12 <133801320+Karthikramesh12@users.noreply.github.com>
2025-02-23 20:21:37 +0530

    added skills section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import LoadingScreen from './Components/Loadin Screen/LoadingScreen'
+import Skills from './Components/Nav/Skills/Skills'
 function App() {
  
 
   return (
     <>
       <LoadingScreen/>
+      <Skills/>
     </>
   )
 }
