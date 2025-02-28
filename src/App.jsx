0e1a5e466f6d7f5dc632e3dc751fa548b15aaf6d
Venkatesh124rayudu <venkateshrayudu124@gmail.com>
2025-03-01 01:07:45 +0530

    UPdated projects

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import Footer from './Components/Footer/Footer'
 import LoadingScreen from './Components/Loadin Screen/LoadingScreen'
 import Projects from './Components/Projects/Projects'
 import Skills from './Components/Skills/Skills'
+import Cv from './Components/Cv/Cv';
 function App() {
  
 
   return (
     <>
       <LoadingScreen/>
+      <Cv/>
       <Skills/>
       <Projects/>
       <Experince/>
